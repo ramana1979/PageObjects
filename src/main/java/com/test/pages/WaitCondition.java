20b9bf7629234171fa896497de02dac0ee4061e2
ramana1979 <58178999+ramana1979@users.noreply.github.com>
2019-11-27 22:40:27 +0530

    add wait class and its method

---
+++ src/main/java/com/test/pages/WaitCondition.java
@@ -0,0 +1,13 @@
+package com.test.pages;
+
+public class WaitCondition {
+	public void waitCondition()
+	{
+		System.out.println("Wait condition class.");
+	}
+	public void fluentWait()
+	{
+		System.out.println("Fluent wait method.");
+	}
+
+}
