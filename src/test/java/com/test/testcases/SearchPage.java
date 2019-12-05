c509c0fb9f915906a4d4fd53d4493f01ae6d7bff
ramana1979 <58178999+ramana1979@users.noreply.github.com>
2019-12-05 10:48:32 +0530

    Add search page

---
+++ src/test/java/com/test/testcases/SearchPage.java
@@ -0,0 +1,24 @@
+package com.test.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SearchPage {
+	@BeforeMethod
+	public void setUp()
+	{
+		System.out.println("Before method is running.");
+	}
+	@Test
+	public void test()
+	{
+		System.out.println("Test method is running.");
+	}
+	@AfterMethod
+	public void endTest()
+	{
+		System.out.println("After method is running.");
+	}
+
+}
